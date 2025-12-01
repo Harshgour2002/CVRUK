ac942f9617af90100b78e692f4e374dab3735fd5
Aayush Rathore <aayu.r.2003@gmail.com>
2025-12-01 11:56:25 +0530

    implemented contact screen for application

--- lib/Widgets/ContactPage.dart
+++ lib/Widgets/ContactPage.dart
@@ -5,11 +5,194 @@ class ContactPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Contact Page",
-        style: TextStyle(fontSize: 22),
-      ),
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+
+                  // University Logo
+                  Center(
+                    child: CircleAvatar(
+                      radius: 80,
+                        backgroundColor: Colors.transparent,
+                        child: Image.asset(
+                          'assets/university-logo.png',
+                          fit: BoxFit.contain,
+                        ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  // Address Container
+                  Container(
+                    width: double.infinity,
+                    //margin: const EdgeInsets.symmetric(horizontal: 20),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.local_activity_outlined, size: 20, color: Colors.amber),
+                            SizedBox(width: 5,),
+                            Text(
+                              "Address",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                            "Village:- Balkhadsura \nPost:- chhaigaon Makhan,"
+                            "Khandwa MP \nPinCode:-450771",
+                          style: TextStyle(fontSize: 16, height: 1.4),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // Phone Number Container
+                  Container(
+                    width: double.infinity,
+                    //margin: const EdgeInsets.symmetric(horizontal: 20),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.call, size: 20, color: Colors.green),
+                            SizedBox(width: 5,),
+                            Text(
+                              "Contact Numbers",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Administrative: 7320-247701\n"
+                              "Admission Enquiry:\n"
+                              "• 9575916565\n"
+                              "• 6269001063\n"
+                              "• 6269001060\n"
+                              "Email: info@cvrukmp.ac.in",
+                          style: TextStyle(fontSize: 16, height: 1.6),
+                        )
+
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // Email Number Container
+                  // Email Number Container
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.email_outlined, size: 20, color: Colors.redAccent),
+                            SizedBox(width: 5,),
+                            Text(
+                              "Email",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+
+                        // Email row with icon
+
+                            SizedBox(width: 10),
+                            Text(
+                              "info@cvrukmp.ac.in",
+                              style: TextStyle(fontSize: 16, height: 1.6),
+                            ),
+                      ],
+                    ),
+                  ),
+
+
+
+
+                  const SizedBox(height: 40),
+
+                  Container(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: (){
+
+                        },
+                        child: Text(
+                            "Generate Inquiry",
+                          style: TextStyle(fontSize: 17),
+                        )
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
